da69e8ba7425d6fd0feeddc5a2a1fd54932c1d54
f4nt0 <fabiantobarn@gmail.com>
2024-05-08 02:26:15 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/cake.png'
 import viteLogo from '/spider.png'
 import './App.css'
@@ -6,15 +6,46 @@ import ReactAudioPlayer from 'react-audio-player';
 
 function App() {
   const [count, setCount] = useState(60)
+  const [rocking, setRocking] = useState(false)
+  const [ligthOn, setLightOn] = useState(true)
+
+  useEffect(() => {
+    if (rocking) {
+      if (ligthOn) {
+        setTimeout(() => {
+          setLightOn(false)
+        }, 200)
+      } else {
+        setTimeout(() => {
+          setLightOn(true)
+        }, 200)
+      }
+    }
+  }, [rocking, ligthOn])
+
+
 
   return (
-    <>
-      <div>
+    <div style={{ backgroundColor: `${ligthOn ? "rgba(86, 15, 88, 0.4)" : "rgba(0, 0, 0, 0.8)"}`, width: "100%", height: "100%", margin: "0px" }}>
+
+
+      <div >
         <ReactAudioPlayer
+          style={{ marginBottom: "10%", marginTop: "20%" }}
           src="/song.mp3"
           autoPlay
           controls
+          onPlay={() => {
+            setTimeout(() => {
+              setRocking(true)
+              console.log("Rpcking?: ", rocking)
+            }, 40000)
+          }}
+          onEnded={() => setRocking(false)}
         />
+      </div>
+      <div>
+        <p>Dale play!</p>
       </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -34,45 +65,45 @@ function App() {
         </p>
       </div>
       <p style={{ color: "#ffffff" }}>
-        Este es sólo el comienzo...<br /><br />
+        Aquí es sólo el comienzo...<br /><br />
 
         En tus ojos <br />
         de cuya oscuridad emanan<br />
         pequeños destellos<br />
         verdes y amarillos,<br />
-        amarillo palido<br /><br />
+        amarillo pálido.<br /><br />
 
         Recorren el espacio<br />
         tales brillos<br />
         y retornan<br />
         en silencio<br />
-        y con frio<br /><br />
+        y con frío.<br /><br />
 
         Se recogen,<br />
         se aprovechan<br />
         de la eternidad,<br />
         chocan contra<br />
-        la substancia<br /><br />
+        la substancia.<br /><br />
 
         Más aún,<br />
         el frio permanece. <br />
         Las llanuras,<br />
         que supiste áridas,<br />
-        están floridas<br /><br />
+        están floridas.<br /><br />
 
         Lloras un poco.<br />
         Más destellos<br />
         rebotan aquí<br />
-        donde tambien<br /> 
+        donde también<br />
         todo comienza<br /><br />
 
-        Que tus años sean cada vez más felices!<br /><br />
+        ¡Que tus años sean cada vez más felices!<br /><br />
 
         The fancat<br /><br />
 
 
       </p>
-    </>
+    </div>
   )
 }
 
